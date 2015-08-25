eeab6d8a77984e7fc98bc1f24b66c3bab427c90f
Amory Meltzer <Amorymeltzer@gmail.com>
2015-08-25 11:23:55 -0700

    Tidy

--- move.pl
+++ move.pl
@@ -4,9 +4,10 @@ use strict;
 use warnings;
 use File::Copy;
 
-open my $raw, '<', 'dupes.txt';
+open my $raw, '<', 'dupes.txt' or die $!;
 
 while (my $nextword = <$raw>) {
   chomp $nextword;
-  move("$nextword","/Users/amorymeltzer/.Trash/");
+  move("$nextword",'/Users/amorymeltzer/.Trash/');
 }
+close $raw or die $!;
