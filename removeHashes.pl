e23d7f06a2d3bc2e885c2a40192a809f8413db29
Amory Meltzer <Amorymeltzer@gmail.com>
2015-08-26 08:00:32 -0700

    Tidy

--- removeHashes.pl
+++ removeHashes.pl
@@ -3,14 +3,15 @@
 use strict;
 use warnings;
 
-open(my $raw, '<', "matchList.txt");
-open( my $output, '>', "dupes.txt");
+open my $raw, '<', 'matchList.txt' or die $!;
+open my $output, '>', 'dupes.txt' or die $!;
 
-while (my $nextword = <$raw>)
-{
-    chomp $nextword;
-#    $nextword =~ m/^(\d+)\w*/i;
-#    my $word = $1.substr($nextword, length($1)+43);
-    my $word = substr($nextword, 43);
-    print $output "$word\n";
+while (my $nextword = <$raw>) {
+  chomp $nextword;
+  #    $nextword =~ m/^(\d+)\w*/i;
+  #    my $word = $1.substr $nextword, length($1)+43;
+  my $word = substr $nextword, 43;
+  print $output "$word\n";
 }
+close $raw or die $!;
+close $output or die $!;
